e76be84755951580595b7324682e71f0ef60f605
shj <josh2kv@gmail.com>
2023-01-16 12:25:39 +0900

    config: open dev server in incognito mode

--- webpack.config.js
+++ webpack.config.js
@@ -6,59 +6,66 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
-const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : "style-loader";
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : "style-loader";
 
 const config = {
-    entry: "./src/index.ts",
+  entry: "./src/index.ts",
 
-    output: {
-        path: path.resolve(__dirname, "dist"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: {
+      app: {
+        name: "chrome",
+        arguments: ["--incognito", "--new-window"],
+      },
     },
-    devServer: {
-        open: true,
-        host: "localhost",
-    },
-    devtool: "source-map",
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "index.html",
-        }),
+    host: "localhost",
+  },
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
 
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/i,
-                loader: "ts-loader",
-                exclude: ["/node_modules/"],
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler, "css-loader"],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: "asset",
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
-    },
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+  },
 };
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = "production";
+  if (isProduction) {
+    config.mode = "production";
 
-        config.plugins.push(new MiniCssExtractPlugin());
-    } else {
-        config.mode = "development";
-    }
-    return config;
+    config.plugins.push(new MiniCssExtractPlugin());
+  } else {
+    config.mode = "development";
+  }
+  return config;
 };
